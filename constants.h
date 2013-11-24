01f49f65d6f5147197a98ef07b5d0a2717fc6e33
JuraX <jurax@jurax.net>
2013-11-24 15:09:30 +0100

    Fixed Errors

--- constants.h
+++ constants.h
@@ -3,7 +3,7 @@
 
 #define HUBBLE_PER_YEAR      6.6760723535E-5 // m/Ma*m
 #define GRAVITATION_CONSTANT 0.155905033     //in lightyear³/sun mass*(Ma)²
-#define SUNMASS              1.98892E30)     //in kg
+#define SUNMASS              1.98892E30      //in kg
 #define PARSEC_TO_LY         3.62
 #define LIGHTYEAR            9.4605284E15
 
